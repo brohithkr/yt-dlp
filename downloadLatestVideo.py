b1a2a987a292e02b3a6e4df926c126e7023e18ae
bannnaiah <rohith7644489@gmail.com>
2023-03-30 00:23:44 +0530

    Added comments

--- downloadLatestVideo.py
+++ downloadLatestVideo.py
@@ -1,26 +1,30 @@
 import requests
-import json
 from bs4 import BeautifulSoup
 from yt_dlp.YoutubeDL import YoutubeDL
+import json
 import sys
 
+"""
+This python script is isolated script which uses yt-dlp to download latest videos of your latest
+videos of your favorite youtuber. It uses channel handle to download latest videos and remembers which 
+latest video was downloaded. If you run this script twice for same channel and no new video was uploaded
+then it will download the first time and show no new video second time. Use this in following ways:
+
+python3 downloadLatestVideos.py -f 'any format codes or specifiers' channel_handle_without_@
 
 
-# ydl = YoutubeDL(params={'format':'[height<=1080]'})
+"""
+
 ydl = None
 params = {}
 for i in range(len(sys.argv)):
     if sys.argv[i]=="-f":
         frm = sys.argv[i+1]
-        # print(frm)
         params['format'] = frm
 
 ydl = YoutubeDL(params=params)
 
-
-
-
-
+# gives id of latest video of given channel handle
 def get_latest_info(channelHandle):
     respose = requests.get(f"https://www.youtube.com/@{channelHandle}/videos")
     soup = BeautifulSoup(respose.text,"html.parser")
@@ -30,6 +34,7 @@ def get_latest_info(channelHandle):
     stvideoid = soup[stindex+len(targetstr):stindex+len(targetstr)+11]
     return stvideoid
 
+# downloads video only once for every channel handle
 def download_latest_video(channelHandle):
     videoid = get_latest_info(channelHandle)
     # downloadedvids = open(f"downloadedVideos_{channelHandle}.txt","r")
@@ -59,12 +64,6 @@ def download_latest_video(channelHandle):
             downloadedvids[channelHandle]=videoid
             json.dump(downloadedvids,vidfile_write,indent=4)
             vidfile_write.close()
-
-    # else:
-    #     print(f"No new {channelHandle} videos to download")
-
-
-
 
 
 if __name__=="__main__":
